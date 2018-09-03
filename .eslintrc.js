761b5a894d79726cddb90e1f5686264069a43912
Wayne Chiu <chaoweichiu@gmail.com>
2018-09-03 20:16:59 +0800

    add lint, exchange api

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "ecmaFeatures": {
+        "modules": true,
+        "module":  true
+    },
+    "rules": {
+        "semi": [1, 'never']
+    }
+};
